ff91596cc7ee5c7f9b45c024be746f32981580d1
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-01-09 21:14:36 +0100

    feat: fork from `reth-revm-inspectors`

---
+++ README.md
@@ -0,0 +1,24 @@
+# evm-inspectors
+
+Common [`revm`] inspector implementations.
+
+Originally part of [`reth`] as the [`reth-revm-inspectors`] crate.
+
+[`revm`]: https://github.com/bluealloy/revm/
+[`reth`]: https://github.com/paradigmxyz/reth/
+[`reth-revm-inspectors`]: https://github.com/paradigmxyz/reth/tree/3fdb24ebd328e9d22d1e6849a3a869e3c4e83485/crates/revm/revm-inspectors/
+
+#### License
+
+<sup>
+Licensed under either of <a href="LICENSE-APACHE">Apache License, Version
+2.0</a> or <a href="LICENSE-MIT">MIT license</a> at your option.
+</sup>
+
+<br>
+
+<sub>
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in these crates by you, as defined in the Apache-2.0 license,
+shall be dual licensed as above, without any additional terms or conditions.
+</sub>
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,24 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+//! revm [Inspector](revm::Inspector) implementations, such as call tracers
+//!
+//! ## Feature Flags
+//!
+//! - `js-tracer`: Enables a JavaScript tracer implementation. This pulls in extra
+//!   dependencies (such as `boa`, `tokio` and `serde_json`).
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/paradigmxyz/reth/main/assets/reth-docs.png",
+    html_favicon_url = "https://avatars0.githubusercontent.com/u/97369466?s=256",
+    issue_tracker_base_url = "https://github.com/paradigmxyz/reth/issues/"
+)]
+#![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+/// An inspector implementation for an EIP2930 Accesslist
+pub mod access_list;
+
+/// An inspector stack abstracting the implementation details of
+/// each inspector and allowing to hook on block/transaction execution,
+/// used in the main RETH executor.
+pub mod stack;
+
+/// An inspector for recording traces
+pub mod tracing;
