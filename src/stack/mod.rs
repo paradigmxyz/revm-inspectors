18d5bf4be08a18fb50388dac2375205b9869024b
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-02-15 13:11:59 +0200

    chore: update call_inspectors macro syntax (#36)
    Matches Foundry:
    
    
    https://github.com/foundry-rs/foundry/blob/8cebc1f581010209eb4aaf325eef3e503d9daba6/crates/evm/evm/src/inspectors/stack.rs#L173-L182
    
    Small breaking change, Reth does not use this macro currently

--- src/stack/mod.rs
+++ src/stack/mod.rs
@@ -85,16 +85,14 @@ pub struct InspectorStackConfig {
 }
 
 /// Helper macro to call the same method on multiple inspectors without resorting to dynamic
-/// dispatch
+/// dispatch.
 #[macro_export]
 macro_rules! call_inspectors {
-    ($id:ident, [ $($inspector:expr),+ ], $call:block) => {
-        $({
-            if let Some($id) = $inspector {
-                $call;
-            }
-        })+
-    }
+    ([$($inspector:expr),+ $(,)?], |$id:ident $(,)?| $call:expr $(,)?) => {{$(
+        if let Some($id) = $inspector {
+            $call
+        }
+    )+}}
 }
 
 impl<DB> Inspector<DB> for InspectorStack
@@ -102,25 +100,25 @@ where
     DB: Database,
 {
     fn initialize_interp(&mut self, interp: &mut Interpreter, context: &mut EvmContext<DB>) {
-        call_inspectors!(inspector, [&mut self.custom_print_tracer], {
+        call_inspectors!([&mut self.custom_print_tracer], |inspector| {
             inspector.initialize_interp(interp, context);
         });
     }
 
     fn step(&mut self, interp: &mut Interpreter, context: &mut EvmContext<DB>) {
-        call_inspectors!(inspector, [&mut self.custom_print_tracer], {
+        call_inspectors!([&mut self.custom_print_tracer], |inspector| {
             inspector.step(interp, context);
         });
     }
 
     fn step_end(&mut self, interp: &mut Interpreter, context: &mut EvmContext<DB>) {
-        call_inspectors!(inspector, [&mut self.custom_print_tracer], {
+        call_inspectors!([&mut self.custom_print_tracer], |inspector| {
             inspector.step_end(interp, context);
         });
     }
 
     fn log(&mut self, context: &mut EvmContext<DB>, log: &Log) {
-        call_inspectors!(inspector, [&mut self.custom_print_tracer], {
+        call_inspectors!([&mut self.custom_print_tracer], |inspector| {
             inspector.log(context, log);
         });
     }
@@ -131,7 +129,7 @@ where
         inputs: &mut CallInputs,
         return_memory_offset: Range<usize>,
     ) -> Option<CallOutcome> {
-        call_inspectors!(inspector, [&mut self.custom_print_tracer], {
+        call_inspectors!([&mut self.custom_print_tracer], |inspector| {
             if let Some(outcome) = inspector.call(context, inputs, return_memory_offset) {
                 return Some(outcome);
             }
@@ -146,7 +144,7 @@ where
         inputs: &CallInputs,
         outcome: CallOutcome,
     ) -> CallOutcome {
-        call_inspectors!(inspector, [&mut self.custom_print_tracer], {
+        call_inspectors!([&mut self.custom_print_tracer], |inspector| {
             let new_ret = inspector.call_end(context, inputs, outcome.clone());
 
             // If the inspector returns a different ret or a revert with a non-empty message,
@@ -164,7 +162,7 @@ where
         context: &mut EvmContext<DB>,
         inputs: &mut CreateInputs,
     ) -> Option<CreateOutcome> {
-        call_inspectors!(inspector, [&mut self.custom_print_tracer], {
+        call_inspectors!([&mut self.custom_print_tracer], |inspector| {
             if let Some(out) = inspector.create(context, inputs) {
                 return Some(out);
             }
@@ -179,7 +177,7 @@ where
         inputs: &CreateInputs,
         outcome: CreateOutcome,
     ) -> CreateOutcome {
-        call_inspectors!(inspector, [&mut self.custom_print_tracer], {
+        call_inspectors!([&mut self.custom_print_tracer], |inspector| {
             let new_ret = inspector.create_end(context, inputs, outcome.clone());
 
             // If the inspector returns a different ret or a revert with a non-empty message,
@@ -193,7 +191,7 @@ where
     }
 
     fn selfdestruct(&mut self, contract: Address, target: Address, value: U256) {
-        call_inspectors!(inspector, [&mut self.custom_print_tracer], {
+        call_inspectors!([&mut self.custom_print_tracer], |inspector| {
             Inspector::<DB>::selfdestruct(inspector, contract, target, value);
         });
     }
