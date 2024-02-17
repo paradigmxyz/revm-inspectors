fb57d266e1fc730bf90ade8ea40d8d419adcac97
tesseract <146037313+DoTheBestToGetTheBest@users.noreply.github.com>
2024-02-17 15:47:01 -0800

    Update stack.rs

--- src/stack.rs
+++ src/stack.rs
@@ -1,10 +1,4 @@
-use alloy_primitives::{Address, Log, B256, U256};
-use revm::{
-    inspectors::CustomPrintTracer,
-    interpreter::{CallInputs, CallOutcome, CreateInputs, CreateOutcome, Interpreter},
-    primitives::Env,
-    Database, EvmContext, Inspector,
-};
+use alloy_primitives::B256;
 use std::fmt::Debug;
 
 /// One can hook on inspector execution in 3 ways:
@@ -22,51 +16,6 @@ pub enum Hook {
     Transaction(B256),
     /// Hooks on every transaction in a block.
     All,
-}
-
-/// An inspector that calls multiple inspectors in sequence.
-///
-/// If a call to an inspector returns a value other than
-/// [revm::interpreter::InstructionResult::Continue] (or equivalent) the remaining inspectors are
-/// not called.
-#[derive(Clone, Default)]
-pub struct InspectorStack {
-    /// An inspector that prints the opcode traces to the console.
-    pub custom_print_tracer: Option<CustomPrintTracer>,
-    /// The provided hook
-    pub hook: Hook,
-}
-
-impl Debug for InspectorStack {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("InspectorStack")
-            .field("custom_print_tracer", &self.custom_print_tracer.is_some())
-            .field("hook", &self.hook)
-            .finish()
-    }
-}
-
-impl InspectorStack {
-    /// Create a new inspector stack.
-    pub fn new(config: InspectorStackConfig) -> Self {
-        let mut stack = Self { hook: config.hook, ..Default::default() };
-
-        if config.use_printer_tracer {
-            stack.custom_print_tracer = Some(CustomPrintTracer::default());
-        }
-
-        stack
-    }
-
-    /// Check if the inspector should be used.
-    pub fn should_inspect(&self, env: &Env, tx_hash: B256) -> bool {
-        match self.hook {
-            Hook::None => false,
-            Hook::Block(block) => env.block.number.to::<u64>() == block,
-            Hook::Transaction(hash) => hash == tx_hash,
-            Hook::All => true,
-        }
-    }
 }
 
 /// Configuration for the inspectors.
@@ -89,105 +38,4 @@ macro_rules! call_inspectors {
             $call
         }
     )+}}
-}
-
-impl<DB> Inspector<DB> for InspectorStack
-where
-    DB: Database,
-{
-    fn initialize_interp(&mut self, interp: &mut Interpreter, context: &mut EvmContext<DB>) {
-        call_inspectors!([&mut self.custom_print_tracer], |inspector| {
-            inspector.initialize_interp(interp, context);
-        });
-    }
-
-    fn step(&mut self, interp: &mut Interpreter, context: &mut EvmContext<DB>) {
-        call_inspectors!([&mut self.custom_print_tracer], |inspector| {
-            inspector.step(interp, context);
-        });
-    }
-
-    fn step_end(&mut self, interp: &mut Interpreter, context: &mut EvmContext<DB>) {
-        call_inspectors!([&mut self.custom_print_tracer], |inspector| {
-            inspector.step_end(interp, context);
-        });
-    }
-
-    fn log(&mut self, context: &mut EvmContext<DB>, log: &Log) {
-        call_inspectors!([&mut self.custom_print_tracer], |inspector| {
-            inspector.log(context, log);
-        });
-    }
-
-    fn call(
-        &mut self,
-        context: &mut EvmContext<DB>,
-        inputs: &mut CallInputs,
-    ) -> Option<CallOutcome> {
-        call_inspectors!([&mut self.custom_print_tracer], |inspector| {
-            if let Some(outcome) = inspector.call(context, inputs) {
-                return Some(outcome);
-            }
-        });
-
-        None
-    }
-
-    fn call_end(
-        &mut self,
-        context: &mut EvmContext<DB>,
-        inputs: &CallInputs,
-        outcome: CallOutcome,
-    ) -> CallOutcome {
-        call_inspectors!([&mut self.custom_print_tracer], |inspector| {
-            let new_ret = inspector.call_end(context, inputs, outcome.clone());
-
-            // If the inspector returns a different ret or a revert with a non-empty message,
-            // we assume it wants to tell us something
-            if new_ret != outcome {
-                return new_ret;
-            }
-        });
-
-        outcome
-    }
-
-    fn create(
-        &mut self,
-        context: &mut EvmContext<DB>,
-        inputs: &mut CreateInputs,
-    ) -> Option<CreateOutcome> {
-        call_inspectors!([&mut self.custom_print_tracer], |inspector| {
-            if let Some(out) = inspector.create(context, inputs) {
-                return Some(out);
-            }
-        });
-
-        None
-    }
-
-    fn create_end(
-        &mut self,
-        context: &mut EvmContext<DB>,
-        inputs: &CreateInputs,
-        outcome: CreateOutcome,
-    ) -> CreateOutcome {
-        call_inspectors!([&mut self.custom_print_tracer], |inspector| {
-            let new_ret = inspector.create_end(context, inputs, outcome.clone());
-
-            // If the inspector returns a different ret or a revert with a non-empty message,
-            // we assume it wants to tell us something
-            if new_ret != outcome {
-                return new_ret;
-            }
-        });
-
-        outcome
-    }
-
-    fn selfdestruct(&mut self, contract: Address, target: Address, value: U256) {
-        call_inspectors!([&mut self.custom_print_tracer], |inspector| {
-            Inspector::<DB>::selfdestruct(inspector, contract, target, value);
-        });
-    }
 }
