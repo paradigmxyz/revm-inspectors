6cf84bb563d31475d3227e44535aff4bc210b07d
tesseract <146037313+DoTheBestToGetTheBest@users.noreply.github.com>
2024-02-18 06:20:29 -0800

    Update opcode.rs

--- src/opcode.rs
+++ src/opcode.rs
@@ -48,14 +48,14 @@ where
     DB: Database,
 {
     fn step(&mut self, interp: &mut Interpreter, context: &mut EvmContext<DB>) {
-        self.gas_inspector.initialize_interp(interp, context);
+        self.initialize_interp(interp, context);
 
         let opcode_value = interp.current_opcode();
         if let Some(opcode) = OpCode::new(opcode_value) {
             *self.opcode_counts.entry(opcode).or_insert(0) += 1;
         }
 
-        self.gas_inspector.step_end(interp, context);
+        self.step_end(interp, context);
         let gas_table = revm::interpreter::instructions::opcode::spec_opcode_gas(context.spec_id());
         let opcode_gas_info = gas_table[opcode_value as usize];
 
@@ -65,6 +65,48 @@ where
             let gas_used = self.gas_inspector.last_gas_cost();
             *self.opcode_gas.entry(opcode).or_insert(0) += gas_used + opcode_gas_cost;
         }
+    }
+
+    fn step_end(&mut self, interp: &mut Interpreter, context: &mut EvmContext<DB>) {
+        self.gas_inspector.step_end(interp, context);
+    }
+    fn call(
+        &mut self,
+        context: &mut EvmContext<DB>,
+        inputs: &mut revm::interpreter::CallInputs,
+    ) -> Option<revm::interpreter::CallOutcome> {
+        self.gas_inspector.call(context, inputs)
+    }
+    fn call_end(
+        &mut self,
+        context: &mut EvmContext<DB>,
+        inputs: &revm::interpreter::CallInputs,
+        outcome: revm::interpreter::CallOutcome,
+    ) -> revm::interpreter::CallOutcome {
+        self.gas_inspector.call_end(context, inputs, outcome)
+    }
+
+    fn create(
+        &mut self,
+        context: &mut EvmContext<DB>,
+        inputs: &mut revm::interpreter::CreateInputs,
+    ) -> Option<revm::interpreter::CreateOutcome> {
+        self.gas_inspector.create(context, inputs)
+    }
+
+    fn create_end(
+        &mut self,
+        context: &mut EvmContext<DB>,
+        inputs: &revm::interpreter::CreateInputs,
+        outcome: revm::interpreter::CreateOutcome,
+    ) -> revm::interpreter::CreateOutcome {
+        self.gas_inspector.create_end(context, inputs, outcome)
+    }
+    fn initialize_interp(&mut self, interp: &mut Interpreter, context: &mut EvmContext<DB>) {
+        self.gas_inspector.initialize_interp(interp, context);
+    }
+    fn log(&mut self, context: &mut EvmContext<DB>, log: &alloy_primitives::Log) {
+        self.gas_inspector.log(context, log)
     }
 }
 
@@ -98,6 +140,34 @@ mod tests {
 
         let opcode_counts = opcode_counter.opcode_counts();
         println!("Opcode Counts: {:?}", opcode_counts);
+
+        let opcode_gas = opcode_counter.opcode_gas();
+        println!("Opcode Gas Usage: {:?}", opcode_gas);
+    }
+    #[test]
+    fn test_with_variety_of_opcodes() {
+        let mut opcode_counter = OpcodeCounterInspector::new();
+        let contract = Box::<Contract>::default();
+        let mut interpreter = Interpreter::new(contract, 2024, false);
+        let db = CacheDB::new(EmptyDB::default());
+
+        let opcodes = [
+            opcode::PUSH1,
+            opcode::PUSH1,
+            opcode::ADD, 
+            opcode::PUSH1,
+            opcode::SSTORE, 
+            opcode::STOP,  
+        ];
+
+        for opcode in opcodes.iter() {
+            interpreter.instruction_pointer = opcode;
+            opcode_counter.step(&mut interpreter, &mut EvmContext::new(db.clone()));
+        }
+
+        let opcode_counts = opcode_counter.opcode_counts();
+
+        println!("{:?}", opcode_counts);
 
         let opcode_gas = opcode_counter.opcode_gas();
         println!("Opcode Gas Usage: {:?}", opcode_gas);
