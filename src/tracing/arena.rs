45062590e58e349374c8f666f0cb1767bd487e40
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-02-19 15:52:22 +0200

    feat: add feature-gated Serde implementations (#47)
    Ref https://github.com/foundry-rs/foundry/issues/6922

--- src/tracing/arena.rs
+++ src/tracing/arena.rs
@@ -1,9 +1,10 @@
-use crate::tracing::types::{CallTrace, CallTraceNode, LogCallOrder};
+use super::types::{CallTrace, CallTraceNode, LogCallOrder};
 
 /// An arena of recorded traces.
 ///
-/// This type will be populated via the [TracingInspector](crate::tracing::TracingInspector).
+/// This type will be populated via the [TracingInspector](super::TracingInspector).
 #[derive(Clone, Debug, PartialEq, Eq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct CallTraceArena {
     /// The arena of recorded trace nodes
     pub(crate) arena: Vec<CallTraceNode>,
