44f95194d97179453abe9bcdc0020391da737d97
jsvisa <delweng@gmail.com>
2024-10-11 22:41:04 +0800

    feat(js/binding): allow owned stack and memory
    Signed-off-by: jsvisa <delweng@gmail.com>

--- src/tracing/js/bindings.rs
+++ src/tracing/js/bindings.rs
@@ -237,9 +237,15 @@ impl StepLog {
 pub(crate) struct MemoryRef(GuardedNullableGc<SharedMemory>);
 
 impl MemoryRef {
-    /// Creates a new stack reference
+    /// Creates a new memory reference
     pub(crate) fn new(mem: &SharedMemory) -> (Self, GcGuard<'_, SharedMemory>) {
         let (inner, guard) = GuardedNullableGc::new_ref(mem);
+        (Self(inner), guard)
+    }
+
+    /// Creates a new owned memory
+    pub(crate) fn new_owned(mem: SharedMemory) -> (Self, GcGuard<'static, SharedMemory>) {
+        let (inner, guard) = GuardedNullableGc::new_owned(mem);
         (Self(inner), guard)
     }
 
@@ -427,6 +433,12 @@ impl StackRef {
     /// Creates a new stack reference
     pub(crate) fn new(stack: &Stack) -> (Self, GcGuard<'_, Stack>) {
         let (inner, guard) = GuardedNullableGc::new_ref(stack);
+        (Self(inner), guard)
+    }
+
+    /// Creates a new owned stack
+    pub(crate) fn new_owned(stack: Stack) -> (Self, GcGuard<'static, Stack>) {
+        let (inner, guard) = GuardedNullableGc::new_owned(stack);
         (Self(inner), guard)
     }
 
