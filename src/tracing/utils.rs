36ad38c550a54c2b883798627e2dffc638087e64
Roman Krasiuk <rokrassyuk@gmail.com>
2025-01-31 11:30:43 +0100

    chore: bump alloy 0.11 (#259)
    ## Description
    
    Bump alloy to the next version.
    
    ---------
    
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- src/tracing/utils.rs
+++ src/tracing/utils.rs
@@ -14,7 +14,7 @@ use revm::{
 ///
 /// See: <https://github.com/ethereum/go-ethereum/blob/366d2169fbc0e0f803b68c042b77b6b480836dbc/eth/tracers/logger/logger.go#L450-L452>
 pub(crate) fn convert_memory(data: &[u8]) -> Vec<String> {
-    let mut memory = Vec::with_capacity((data.len() + 31) / 32);
+    let mut memory = Vec::with_capacity(data.len().div_ceil(32));
     let chunks = data.chunks_exact(32);
     let remainder = chunks.remainder();
     for chunk in chunks {
