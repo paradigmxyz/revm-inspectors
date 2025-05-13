f0f15f7daf81d4cfff44146abca3a3fdfa5efdbc
kustrun <domen.balantic1@gmail.com>
2025-05-13 19:32:20 +0200

    feat(mux-tracer): Support JS tracer in MUX tracer.
    Ref: https://github.com/paradigmxyz/revm-inspectors/issues/58

--- src/tracing/js/mod.rs
+++ src/tracing/js/mod.rs
@@ -54,6 +54,8 @@ pub const RECURSION_LIMIT: usize = 10_000;
 #[derive(Debug)]
 pub struct JsInspector {
     ctx: Context,
+    /// The javascript code provided to the inspector.
+    code: String,
     /// The javascript config provided to the inspector.
     _js_config_value: JsValue,
     /// The input config object.
@@ -126,9 +128,10 @@ impl JsInspector {
         register_builtins(&mut ctx)?;
 
         // evaluate the code
-        let code = format!("({code})");
-        let obj =
-            ctx.eval(Source::from_bytes(code.as_bytes())).map_err(JsInspectorError::EvalCode)?;
+        let code_to_evaluate = format!("({code})");
+        let obj = ctx
+            .eval(Source::from_bytes(code_to_evaluate.as_bytes()))
+            .map_err(JsInspectorError::EvalCode)?;
 
         let obj = obj.as_object().cloned().ok_or(JsInspectorError::ExpectedJsObject)?;
 
@@ -178,6 +181,7 @@ impl JsInspector {
 
         Ok(Self {
             ctx,
+            code,
             _js_config_value,
             config,
             obj,
@@ -190,6 +194,11 @@ impl JsInspector {
             call_stack: Default::default(),
             precompiles_registered: false,
         })
+    }
+
+    /// Returns the javascript code.
+    pub const fn code(&self) -> &String {
+        &self.code
     }
 
     /// Returns the config object.
@@ -219,7 +228,7 @@ impl JsInspector {
     /// Note: This is supposed to be called after the inspection has finished.
     pub fn json_result<DB>(
         &mut self,
-        res: ResultAndState<impl HaltReasonTr>,
+        res: &ResultAndState<impl HaltReasonTr>,
         tx: &impl Transaction,
         block: &impl Block,
         db: &DB,
@@ -235,7 +244,7 @@ impl JsInspector {
     /// Calls the result function and returns the result.
     pub fn result<TX, DB>(
         &mut self,
-        res: ResultAndState<impl HaltReasonTr>,
+        res: &ResultAndState<impl HaltReasonTr>,
         tx: &TX,
         block: &impl Block,
         db: &DB,
@@ -258,7 +267,7 @@ impl JsInspector {
                     output_bytes = Some(out);
                 }
                 Output::Create(out, addr) => {
-                    to = addr;
+                    to = addr.clone();
                     output_bytes = Some(out);
                 }
             },
@@ -293,7 +302,7 @@ impl JsInspector {
             value: tx.value(),
             block: block.number(),
             coinbase: block.beneficiary(),
-            output: output_bytes.unwrap_or_default(),
+            output: output_bytes.unwrap_or_default().clone(),
             time: block.timestamp().to_string(),
             intrinsic_gas: 0,
             transaction_ctx: self.transaction_context,
@@ -718,7 +727,7 @@ mod tests {
 
         assert_eq!(res.result.is_success(), success);
         let (ctx, inspector) = evm.ctx_inspector();
-        inspector.json_result(res, ctx.tx(), ctx.block(), ctx.db_ref()).unwrap()
+        inspector.json_result(&res, ctx.tx(), ctx.block(), ctx.db_ref()).unwrap()
     }
 
     #[test]
--- src/tracing/mux.rs
+++ src/tracing/mux.rs
@@ -1,13 +1,17 @@
-use crate::tracing::{FourByteInspector, TracingInspector, TracingInspectorConfig};
+use crate::tracing::{
+    js::{JsInspector, JsInspectorError},
+    FourByteInspector, TracingInspector, TracingInspectorConfig,
+};
 use alloc::vec::Vec;
 use alloy_primitives::{map::HashMap, Address, Log, U256};
 use alloy_rpc_types_eth::TransactionInfo;
 use alloy_rpc_types_trace::geth::{
     mux::{MuxConfig, MuxFrame},
-    CallConfig, FlatCallConfig, FourByteFrame, GethDebugBuiltInTracerType, NoopFrame,
-    PreStateConfig,
+    CallConfig, FlatCallConfig, FourByteFrame, GethDebugBuiltInTracerType, GethDebugTracerType,
+    GethTrace, NoopFrame, PreStateConfig,
 };
 use revm::{
+    context::{Block, Transaction},
     context_interface::{
         result::{HaltReasonTr, ResultAndState},
         ContextTr,
@@ -21,10 +25,13 @@ use revm::{
 use thiserror::Error;
 
 /// Mux tracing inspector that runs and collects results of multiple inspectors at once.
-#[derive(Clone, Debug)]
+#[derive(Debug)]
+#[cfg(feature = "js-tracer")]
 pub struct MuxInspector {
     /// An instance of FourByteInspector that can be reused
     four_byte: Option<FourByteInspector>,
+    /// An instance of JsInspector that can be reused
+    js_tracer: Option<JsInspector>,
     /// An instance of TracingInspector that can be reused
     tracing: Option<TracingInspector>,
     /// Configurations for different Geth trace types
@@ -44,70 +51,100 @@ impl MuxInspector {
     /// Try creating a new instance of [MuxInspector] from the given [MuxConfig].
     pub fn try_from_config(config: MuxConfig) -> Result<MuxInspector, Error> {
         let mut four_byte = None;
+        let mut js_tracer = None;
         let mut inspector_config = TracingInspectorConfig::none();
         let mut configs = Vec::new();
 
         // Process each tracer configuration
         for (tracer_type, tracer_config) in config.0 {
             match tracer_type {
-                GethDebugBuiltInTracerType::FourByteTracer => {
-                    if tracer_config.is_some() {
-                        return Err(Error::UnexpectedConfig(tracer_type));
+                GethDebugTracerType::BuiltInTracer(built_in_tracer_type) => {
+                    match built_in_tracer_type {
+                        GethDebugBuiltInTracerType::FourByteTracer => {
+                            if tracer_config.is_some() {
+                                return Err(Error::UnexpectedConfig(tracer_type));
+                            }
+                            four_byte = Some(FourByteInspector::default());
+                        }
+                        GethDebugBuiltInTracerType::CallTracer => {
+                            let call_config = tracer_config
+                                .ok_or(Error::MissingConfig(built_in_tracer_type))?
+                                .into_call_config()?;
+
+                            inspector_config
+                                .merge(TracingInspectorConfig::from_geth_call_config(&call_config));
+                            configs.push((built_in_tracer_type, TraceConfig::Call(call_config)));
+                        }
+                        GethDebugBuiltInTracerType::PreStateTracer => {
+                            let prestate_config = tracer_config
+                                .ok_or(Error::MissingConfig(built_in_tracer_type))?
+                                .into_pre_state_config()?;
+
+                            inspector_config.merge(
+                                TracingInspectorConfig::from_geth_prestate_config(&prestate_config),
+                            );
+                            configs.push((
+                                built_in_tracer_type,
+                                TraceConfig::PreState(prestate_config),
+                            ));
+                        }
+                        GethDebugBuiltInTracerType::NoopTracer => {
+                            if tracer_config.is_some() {
+                                return Err(Error::UnexpectedConfig(tracer_type));
+                            }
+                            configs.push((built_in_tracer_type, TraceConfig::Noop));
+                        }
+                        GethDebugBuiltInTracerType::FlatCallTracer => {
+                            let flatcall_config = tracer_config
+                                .ok_or(Error::MissingConfig(built_in_tracer_type))?
+                                .into_flat_call_config()?;
+
+                            inspector_config.merge(TracingInspectorConfig::from_flat_call_config(
+                                &flatcall_config,
+                            ));
+                            configs.push((
+                                built_in_tracer_type,
+                                TraceConfig::FlatCall(flatcall_config),
+                            ));
+                        }
+                        GethDebugBuiltInTracerType::MuxTracer => {
+                            return Err(Error::UnexpectedConfig(tracer_type));
+                        }
                     }
-                    four_byte = Some(FourByteInspector::default());
                 }
-                GethDebugBuiltInTracerType::CallTracer => {
-                    let call_config = tracer_config
-                        .ok_or(Error::MissingConfig(tracer_type))?
-                        .into_call_config()?;
+                GethDebugTracerType::JsTracer(ref code) => {
+                    let config = match tracer_config {
+                        Some(config) => config.into_json(),
+                        None => serde_json::Value::Null,
+                    };
 
-                    inspector_config
-                        .merge(TracingInspectorConfig::from_geth_call_config(&call_config));
-                    configs.push((tracer_type, TraceConfig::Call(call_config)));
-                }
-                GethDebugBuiltInTracerType::PreStateTracer => {
-                    let prestate_config = tracer_config
-                        .ok_or(Error::MissingConfig(tracer_type))?
-                        .into_pre_state_config()?;
-
-                    inspector_config
-                        .merge(TracingInspectorConfig::from_geth_prestate_config(&prestate_config));
-                    configs.push((tracer_type, TraceConfig::PreState(prestate_config)));
-                }
-                GethDebugBuiltInTracerType::NoopTracer => {
-                    if tracer_config.is_some() {
-                        return Err(Error::UnexpectedConfig(tracer_type));
-                    }
-                    configs.push((tracer_type, TraceConfig::Noop));
-                }
-                GethDebugBuiltInTracerType::FlatCallTracer => {
-                    let flatcall_config = tracer_config
-                        .ok_or(Error::MissingConfig(tracer_type))?
-                        .into_flat_call_config()?;
-
-                    inspector_config
-                        .merge(TracingInspectorConfig::from_flat_call_config(&flatcall_config));
-                    configs.push((tracer_type, TraceConfig::FlatCall(flatcall_config)));
-                }
-                GethDebugBuiltInTracerType::MuxTracer => {
-                    return Err(Error::UnexpectedConfig(tracer_type));
+                    js_tracer = Some(JsInspector::new(code.clone(), config)?);
                 }
             }
         }
 
         let tracing = (!configs.is_empty()).then(|| TracingInspector::new(inspector_config));
 
-        Ok(MuxInspector { four_byte, tracing, configs })
+        Ok(MuxInspector { four_byte, js_tracer, tracing, configs })
     }
 
     /// Try converting this [MuxInspector] into a [MuxFrame].
     pub fn try_into_mux_frame<DB: DatabaseRef>(
-        &self,
+        &mut self,
         result: &ResultAndState<impl HaltReasonTr>,
+        tx: &impl Transaction,
+        block: &impl Block,
         db: &DB,
-        tx_info: TransactionInfo,
     ) -> Result<MuxFrame, DB::Error> {
         let mut frame = HashMap::with_capacity_and_hasher(self.configs.len(), Default::default());
+
+        let tx_info = TransactionInfo {
+            block_number: Some(block.number()),
+            base_fee: Some(block.basefee()),
+            hash: None,
+            block_hash: None,
+            index: None,
+        };
 
         for (tracer_type, config) in &self.configs {
             let trace = match config {
@@ -145,14 +182,26 @@ impl MuxInspector {
                 TraceConfig::Noop => NoopFrame::default().into(),
             };
 
-            frame.insert(*tracer_type, trace);
+            frame.insert(GethDebugTracerType::BuiltInTracer(*tracer_type), trace);
         }
 
         // Add four byte trace if inspector exists
         if let Some(inspector) = &self.four_byte {
             frame.insert(
-                GethDebugBuiltInTracerType::FourByteTracer,
+                GethDebugTracerType::BuiltInTracer(GethDebugBuiltInTracerType::FourByteTracer),
                 FourByteFrame::from(inspector).into(),
+            );
+        }
+
+        // Add js tracer if inspector exists
+        if let Some(ref mut js_inspector) = self.js_tracer {
+            frame.insert(
+                GethDebugTracerType::JsTracer(js_inspector.code().clone()),
+                GethTrace::JS(
+                    js_inspector
+                        .json_result(result, tx, block, db)
+                        .unwrap_or_else(|err| serde_json::json!({ "error": err.to_string() })),
+                ),
             );
         }
 
@@ -162,7 +211,7 @@ impl MuxInspector {
 
 impl<CTX> Inspector<CTX> for MuxInspector
 where
-    CTX: ContextTr<Journal: JournalExt>,
+    CTX: ContextTr<Journal: JournalExt, Db: DatabaseRef>,
 {
     #[inline]
     fn initialize_interp(&mut self, interp: &mut Interpreter, context: &mut CTX) {
@@ -170,6 +219,9 @@ where
             inspector.initialize_interp(interp, context);
         }
         if let Some(ref mut inspector) = self.tracing {
+            inspector.initialize_interp(interp, context);
+        }
+        if let Some(ref mut inspector) = self.js_tracer {
             inspector.initialize_interp(interp, context);
         }
     }
@@ -182,6 +234,9 @@ where
         if let Some(ref mut inspector) = self.tracing {
             inspector.step(interp, context);
         }
+        if let Some(ref mut inspector) = self.js_tracer {
+            inspector.step(interp, context);
+        }
     }
 
     #[inline]
@@ -192,6 +247,9 @@ where
         if let Some(ref mut inspector) = self.tracing {
             inspector.step_end(interp, context);
         }
+        if let Some(ref mut inspector) = self.js_tracer {
+            inspector.step_end(interp, context);
+        }
     }
 
     #[inline]
@@ -200,6 +258,9 @@ where
             inspector.log(interp, context, log.clone());
         }
         if let Some(ref mut inspector) = self.tracing {
+            inspector.log(interp, context, log.clone());
+        }
+        if let Some(ref mut inspector) = self.js_tracer {
             inspector.log(interp, context, log);
         }
     }
@@ -210,6 +271,9 @@ where
             let _ = inspector.call(context, inputs);
         }
         if let Some(ref mut inspector) = self.tracing {
+            let _ = inspector.call(context, inputs);
+        }
+        if let Some(ref mut inspector) = self.js_tracer {
             return inspector.call(context, inputs);
         }
         None
@@ -223,6 +287,9 @@ where
         if let Some(ref mut inspector) = self.tracing {
             inspector.call_end(context, inputs, outcome);
         }
+        if let Some(ref mut inspector) = self.js_tracer {
+            inspector.call_end(context, inputs, outcome);
+        }
     }
 
     #[inline]
@@ -231,6 +298,9 @@ where
             let _ = inspector.create(context, inputs);
         }
         if let Some(ref mut inspector) = self.tracing {
+            let _ = inspector.create(context, inputs);
+        }
+        if let Some(ref mut inspector) = self.js_tracer {
             return inspector.create(context, inputs);
         }
         None
@@ -249,6 +319,9 @@ where
         if let Some(ref mut inspector) = self.tracing {
             inspector.create_end(context, inputs, outcome);
         }
+        if let Some(ref mut inspector) = self.js_tracer {
+            inspector.create_end(context, inputs, outcome);
+        }
     }
 
     #[inline]
@@ -261,6 +334,9 @@ where
             let _ = inspector.eofcreate(context, inputs);
         }
         if let Some(ref mut inspector) = self.tracing {
+            let _ = inspector.eofcreate(context, inputs);
+        }
+        if let Some(ref mut inspector) = self.js_tracer {
             return inspector.eofcreate(context, inputs);
         }
         None
@@ -279,6 +355,9 @@ where
         if let Some(ref mut inspector) = self.tracing {
             inspector.eofcreate_end(context, inputs, outcome);
         }
+        if let Some(ref mut inspector) = self.js_tracer {
+            inspector.eofcreate_end(context, inputs, outcome);
+        }
     }
 
     #[inline]
@@ -289,6 +368,9 @@ where
         if let Some(ref mut inspector) = self.tracing {
             <TracingInspector as Inspector<CTX>>::selfdestruct(inspector, contract, target, value);
         }
+        if let Some(ref mut inspector) = self.js_tracer {
+            <JsInspector as Inspector<CTX>>::selfdestruct(inspector, contract, target, value);
+        }
     }
 }
 
@@ -297,11 +379,14 @@ where
 pub enum Error {
     /// Config was provided for a tracer that does not expect it
     #[error("unexpected config for tracer '{0:?}'")]
-    UnexpectedConfig(GethDebugBuiltInTracerType),
+    UnexpectedConfig(GethDebugTracerType),
     /// Expected config is missing
     #[error("expected config is missing for tracer '{0:?}'")]
     MissingConfig(GethDebugBuiltInTracerType),
     /// Error when deserializing the config
     #[error("error deserializing config: {0}")]
     InvalidConfig(#[from] serde_json::Error),
+    /// Error when creating the JS inspector
+    #[error("failed to create JS inspector: {0}")]
+    JsInspectorErr(#[from] JsInspectorError),
 }
