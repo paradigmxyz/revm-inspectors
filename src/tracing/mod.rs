beb88cd9d9d67a072e4b5b8c723088ee43910be5
zerosnacks <95942363+zerosnacks@users.noreply.github.com>
2025-06-05 19:52:49 +0200

    chore: remove EOF leftovers (#301)
    Closes: https://github.com/paradigmxyz/revm-inspectors/issues/174
    (replaces it)
    
    Follow up of https://github.com/paradigmxyz/revm-inspectors/pull/299
    
    Removes `code_section_idx` that is EOF specific, as added in
    https://github.com/paradigmxyz/revm-inspectors/pull/169
    
    Keeps: `record_immediate_bytes` as I think it may be useful / still used
    by Foundry / is more readable.
    
    Undo's missed `ExtDelegateCall` from #299 :
    https://github.com/paradigmxyz/revm-inspectors/pull/156
    
    Ref:
    https://github.com/paradigmxyz/revm-inspectors/pulls?q=is%3Apr+eof+is%3Aclosed

--- src/tracing/mod.rs
+++ src/tracing/mod.rs
@@ -17,9 +17,7 @@ use revm::{
     context_interface::ContextTr,
     inspector::JournalExt,
     interpreter::{
-        interpreter_types::{
-            Immediates, InputsTr, Jumps, LoopControl, ReturnData, RuntimeFlag, SubRoutineStack,
-        },
+        interpreter_types::{Immediates, InputsTr, Jumps, LoopControl, ReturnData, RuntimeFlag},
         CallInput, CallInputs, CallOutcome, CallScheme, CreateInputs, CreateOutcome,
         InstructionResult, Interpreter, InterpreterResult,
     },
@@ -451,7 +449,6 @@ impl TracingInspector {
         trace.trace.steps.push(CallTraceStep {
             depth: context.journal().depth() as u64,
             pc: interp.bytecode.pc(),
-            code_section_idx: interp.sub_routine.routine_idx(),
             op,
             contract: interp.input.target_address(),
             stack,
@@ -567,23 +564,22 @@ where
     fn call(&mut self, context: &mut CTX, inputs: &mut CallInputs) -> Option<CallOutcome> {
         // determine correct `from` and `to` based on the call scheme
         let (from, to) = match inputs.scheme {
-            CallScheme::DelegateCall | CallScheme::CallCode | CallScheme::ExtDelegateCall => {
+            CallScheme::DelegateCall | CallScheme::CallCode => {
                 (inputs.target_address, inputs.bytecode_address)
             }
             _ => (inputs.caller, inputs.target_address),
         };
 
-        let value =
-            if matches!(inputs.scheme, CallScheme::DelegateCall | CallScheme::ExtDelegateCall) {
-                // for delegate calls we need to use the value of the top trace
-                if let Some(parent) = self.active_trace() {
-                    parent.trace.value
-                } else {
-                    inputs.call_value()
-                }
+        let value = if matches!(inputs.scheme, CallScheme::DelegateCall) {
+            // for delegate calls we need to use the value of the top trace
+            if let Some(parent) = self.active_trace() {
+                parent.trace.value
             } else {
                 inputs.call_value()
-            };
+            }
+        } else {
+            inputs.call_value()
+        };
 
         // if calls to precompiles should be excluded, check whether this is a call to a precompile
         let maybe_precompile = self
