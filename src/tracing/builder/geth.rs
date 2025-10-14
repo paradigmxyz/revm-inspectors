9c2fd43fc7863bf3e3d38bb8ea80aea3b618fb90
Arsenii Kulikov <klkvrr@gmail.com>
2025-10-14 20:20:07 +0400

    fix: use correct depth for geth traces (#369)
    follow-up for https://github.com/paradigmxyz/revm-inspectors/pull/355

--- src/tracing/builder/geth.rs
+++ src/tracing/builder/geth.rs
@@ -79,7 +79,10 @@ impl<'a> GethTraceBuilder<'a> {
         while let Some(CallTraceStepStackItem { trace_node, step, call_child_id }) =
             step_stack.pop_back()
         {
-            let mut log = step.convert_to_geth_struct_log(opts, trace_node.trace.depth as u64);
+            // We increment the depth by one because steps that are part of call at depth N should
+            // have depth N + 1. For example, steps inside of a top-level call should
+            // have depth 1.
+            let mut log = step.convert_to_geth_struct_log(opts, trace_node.trace.depth as u64 + 1);
 
             // Fill in memory and storage depending on the options
             if opts.is_storage_enabled() {
